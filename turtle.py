7b023ce370cb4f5aa030f78ab920de7bc978ead6
Anya <github@mail.com>
2023-01-06 20:46:21 +0300

    написала класс медуз

--- turtle.py
+++ turtle.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pygame
+import random
 
 # функция проверки файла на его наличие
 def load_image(name, colorkey=None):
@@ -12,10 +13,25 @@ def load_image(name, colorkey=None):
     image = pygame.image.load(fullname)
     return image
 
+# класс медуз
+class Jellyfish(pygame.sprite.Sprite):
+    def __init__(self, *group):
+        super().__init__(jellyfish_group, all_sprites)
+        self.image = food_image
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.x = random.randrange(5, width - 5)
+        self.rect.y = height
+
+    def update(self):
+        self.rect.y -= 1
+        self.rect = self.image.get_rect().move(self.rect.x, self.rect.y)
+
+
 pygame.init()
 # параментры окна
-width = 1000
-height = 800
+width = 1400
+height = 950
 size = width, height
 screen = pygame.display.set_mode(size)
 # группы спрайтов
@@ -27,13 +43,22 @@ player_image = load_image('turtle.png')
 food_image = load_image('медуза.png')
 background = load_image('sea.jpg')
 clock = pygame.time.Clock()
+
+jellyfish = Jellyfish()
+# объявление своего события
+MYEVENTTYPE = pygame.USEREVENT + 1
+time = random.randrange(1000, 10000)
 running = True
 # основной цикл
 while running:
     # цикл обработки событий
     for event in pygame.event.get():
+        pygame.time.set_timer(MYEVENTTYPE, time)
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == MYEVENTTYPE:
+            time = random.randrange(1000, 10000)
+            Jellyfish(jellyfish_group)
     screen.blit(background, (0, 0))
     player_group.draw(screen)
     jellyfish_group.draw(screen)
