3b213246d3dc5bec7ae2febc497ed29e694e2fbc
ciborg228 <voigva@yandex.ru>
2023-01-08 18:47:42 +0300

    добавил окно и кнопку. теперь программа, спустя два дня, рисует платформу, и кубик, ох, блин, мне же ещё приыжок добавлять... не успеваю, жалко -_-

--- kybik.py
+++ kybik.py
@@ -6,9 +6,8 @@ pygame.init()
 display_width = 900
 display_height = 600
 display = pygame.display.set_mode((display_width, display_height))
-pygame.display.set_caption("КУУУУУБИК")
+pygame.display.set_caption("Ну допустим, недодинозаврик")
 
-# PlayerCharacher
 x = 80
 y = display_height / 2 + 140
 width = 40
@@ -92,14 +91,23 @@ def Main():
             if event.type == pygame.QUIT:
                 pygame.quit()
 
+        if ceck_collision(CactusArr):
+            working = False
+            game_over()
+            break
+
         mouse_click = pygame.mouse.get_pressed()
-        scoreText = font.render("БАЛЫ ЗА ОГЕ: " + str(score), 1, (0, 0, 0))
+        scoreText = font.render("твои балы за ОГЭ " + str(score), 1, (0, 0, 0))
+
+        count_scores(CactusArr)
+
         if mouse_click[0]:
             MakeJump = True
 
         display.fill((255, 255, 255))
         pygame.draw.rect(display, (255, 0, 0), (x, y, width, height))
         pygame.draw.rect(display, (0, 255, 0), (0, display_height - 80, display_width, 80))
+        draw_cactus(CactusArr)
         display.blit(scoreText, (0, 0))
         pygame.display.update()
         clock.tick(80)
@@ -107,6 +115,72 @@ def Main():
 
 
 
+def draw_cactus(array):
+    for cactus in array:
+        check = cactus.draw()
+
+
+
+def ceck_collision(barriers):
+    global x, width
+
+    for barrier in barriers:
+        if y + height >= barrier.y:
+            if barrier.x <= x <= barrier.x + barrier.width:
+                return True
+            elif barrier.x <= x + width <= barrier.x + barrier.width:
+                return True
+
+    return False
+
+
+def count_scores(barriers):
+    global score, above_cactus
+
+    if not above_cactus:
+        for barrier in barriers:
+            if barrier.x <= x + width / 2 <= barrier.x + barrier.width:
+                if y + height - 5 <= barrier.y:
+                    above_cactus = True
+                    break
+    else:
+        if JumpCount == -28:
+            score += 1
+            above_cactus = False
+
+
+def game_over():
+    working = True
+    restart_button = Button(100, 50, pygame.image.load("data/restart-button-inactive.png"),
+                            pygame.image.load("data/restart-button-active.png"))
+    menu_button = Button(100, 50, pygame.image.load("data/menu-button-inactive.png"),
+                         pygame.image.load("data/menu-button-active.png"))
+
+    while working:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        info = font.render("you dead :(", 1, (0, 0, 0))
+        keys = pygame.key.get_pressed()
+
+        if restart_button.action == True:
+            working = False
+            Main()
+            restart_button.action = False
+
+        if menu_button.action == True:
+            working = False
+            Menu()
+
+        display.fill((0, 255, 255))
+        display.blit(info, (display_width / 2 - 80, 20))
+
+        restart_button.draw(display_width / 2 - restart_button.width, display_height / 2 - restart_button.height)
+        menu_button.draw(display_width / 2 - restart_button.width, display_height / 2 - restart_button.height + 40)
+
+        pygame.display.update()
+        clock.tick(60)
 
 
 def Menu():
@@ -121,7 +195,7 @@ def Menu():
             if event.type == pygame.QUIT:
                 pygame.quit()
 
-        info = font.render("Jumping Cube :)", 1, (0, 0, 0))
+        info = font.render(" ", 1, (0, 0, 0))
         keys = pygame.key.get_pressed()
 
         if play_button.action == True:
