4a0aad678721eff6e93c60746256e3b0ee26a9ea
Anya <github@mail.com>
2023-01-10 16:43:05 +0300

    изменила счетчик и классы, добавила рост рыбы

--- fish_eat_fish.py
+++ fish_eat_fish.py
@@ -13,49 +13,58 @@ def load_image(name, colorkey=None):
     image = pygame.image.load(fullname)
     return image
 
-# класс медуз
-class Jellyfish(pygame.sprite.Sprite):
+# класс маленькой рыбы
+class SmallFish(pygame.sprite.Sprite):
     def __init__(self, *group):
-        super().__init__(jellyfish_group, all_sprites)
+        super().__init__(small_fish_group, all_sprites)
         self.image = food_image
+        self.image = pygame.transform.scale(self.image, (15, 10))
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
-        self.rect.x = random.randrange(30, width - self.image.get_width() - 30)
-        self.rect.y = height
+        self.rect.x = width
+        self.rect.y = random.randrange(0, height - self.image.get_height())
 
     def update(self):
         global count
-        self.rect.y -= 1
-        for i in jellyfish_group:
-            if pygame.sprite.collide_mask(self, turtle):
-                count += 1
+        self.rect.x -= 1
+        for i in small_fish_group:
+            # после столкновения маленькая рыбка исчезает
+            # счет прибавляется
+            if pygame.sprite.collide_mask(self, player):
                 self.kill()
+                count += 1
+                change()
+            break
 
-# класс черепахи
-class Turtle(pygame.sprite.Sprite):
+# класс игрока
+class Player(pygame.sprite.Sprite):
     def __init__(self, *group):
         super().__init__(player_group, all_sprites)
         self.pos = width // 2, height // 2
-        self.image = player_image
-        self.mask = pygame.mask.from_surface(self.image)
+        self.image = im
+        self.mask = im_mask
         self.rect = self.image.get_rect().move(self.pos)
+
+    def grow(self):
+        global im_w
+        im_w = im.get_width() + 20
 
     def move(self, x_pos, y_pos):
         self.pos = x_pos, y_pos
         self.rect = self.image.get_rect().move(x_pos, y_pos)
 
-    def update(self, turtle, keys):
-        x, y = turtle.pos
+    def update(self, player, keys):
+        x, y = player.pos
         if keys[pygame.K_LEFT]:
-            self.image = player_image
+            self.image = im
             # проверка на выход за левые границы
             if x - 5 >= 0:
                 x -= 5
             else:
                 x = 0
-        elif keys[pygame.K_RIGHT]:
+        if keys[pygame.K_RIGHT]:
             # поворот изображения
-            self.image = pygame.transform.flip(player_image, player_image.get_width(), 0)
+            self.image = pygame.transform.flip(im, im.get_width(), 0)
             # проверка на выход за правые границы
             d_width = width - self.image.get_width()
             if x + 5 <= d_width:
@@ -63,25 +72,35 @@ class Turtle(pygame.sprite.Sprite):
             else:
                 x = d_width
         if keys[pygame.K_DOWN]:
+            self.image = pygame.transform.scale(self.image, (im_w, im_w))
+            self.mask = pygame.mask.from_surface(im)
             # проверка на выход за нижние границы
             d_height = height - self.image.get_height()
             if y + 5 <= d_height:
                 y += 5
             else:
                 y = d_height
-        elif keys[pygame.K_UP]:
+        if keys[pygame.K_UP]:
+            self.image = pygame.transform.scale(self.image, (im_w, im_w))
+            self.mask = pygame.mask.from_surface(im)
             # проверка на выход за верхние границы
             if y - 5 >= 0:
                 y -= 5
             else:
                 y = 0
-        turtle.move(x, y)
+        player.move(x, y)
 
 # функция отрисовки счета
 def draw_count():
     font = pygame.font.Font(None, 50)
-    title = font.render(str(count), True, (100, 255, 100))
+    title = font.render('score:' + ' ' + str(count), True, (0, 0, 0), (66, 170, 255))
     screen.blit(title, (0, 0))
+
+def change():
+    if count == 5:
+        player.grow()
+    if count == 10:
+        player.grow()
 
 pygame.init()
 # параментры окна
@@ -91,39 +110,42 @@ size = width, height
 screen = pygame.display.set_mode(size)
 # группы спрайтов
 all_sprites = pygame.sprite.Group()
-jellyfish_group = pygame.sprite.Group()
+small_fish_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
 # загруска изображений
-player_image = load_image('turtle.png')
-food_image = load_image('медуза.png')
+player_image = load_image('blue_fish.png')
+food_image = load_image('small_fish.png')
 background = load_image('sea.jpg')
+im = player_image
+im_w = player_image.get_width()
+im_mask = pygame.mask.from_surface(im)
 clock = pygame.time.Clock()
 # переменная счетчик
 count = 0
 # переменые классов
-turtle = Turtle()
-jellyfish = Jellyfish()
+player = Player()
+small_fish = SmallFish()
 # объявление своего события
 MYEVENTTYPE = pygame.USEREVENT + 1
-time = random.randrange(1900, 10000)
 running = True
 # основной цикл
 while running:
     # цикл обработки событий
     for event in pygame.event.get():
-        pygame.time.set_timer(MYEVENTTYPE, time)
+        pygame.time.set_timer(MYEVENTTYPE, 1900)
         if event.type == pygame.QUIT:
             running = False
         elif event.type == MYEVENTTYPE:
-            time = random.randrange(1900, 10000)
-            Jellyfish(jellyfish_group)
+            SmallFish(small_fish_group)
+    im = pygame.transform.scale(player_image, (im_w, im_w))
+    im_mask = pygame.mask.from_surface(im)
     # получение состояния кнопок
     keys = pygame.key.get_pressed()
-    turtle.update(turtle, keys)
-    jellyfish_group.update()
+    player.update(player, keys)
+    small_fish_group.update()
     screen.blit(background, (0, 0))
     draw_count()
-    jellyfish_group.draw(screen)
+    small_fish_group.draw(screen)
     player_group.draw(screen)
     pygame.display.flip()
     clock.tick(60)
